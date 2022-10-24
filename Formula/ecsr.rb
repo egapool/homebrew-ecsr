1428f708ad69ea075acdcc90a037f220d33c2c85
Yusuke Egami <egpersent@gmail.com>
2022-10-24 22:28:04 +0900

    edit

--- Formula/ecsr.rb
+++ Formula/ecsr.rb
@@ -3,7 +3,6 @@ class Ecsr < Formula
   homepage "https://github.com/egapool/ecsr"
   url "https://github.com/egapool/ecsr/releases/download/v0.1.0/ecsr-v0.1.0-aarch64-apple-darwin.tar.gz"
   sha256 "ac69b44971dfdbbf9c6abcc6930bc1b8f8d2683fd63bf0958f6b96ce4bc0d30c"
-  version "v0.1.0"
 
   def install
     bin.install "escr"
